32ae1524ea8b5a0293a17d6284a018cb3693fab9
shiva <shiva.aws@gmail.com>
2025-06-24 17:08:35 +0530

    infra configuration via terraform

---
+++ practice-ec2.tf/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "this" {
+  #count = 3
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
+  disable_api_termination              = false
+  instance_initiated_shutdown_behavior = "stop"  # Can be "terminate" or "stop"
+  tags = {
+    Name    = "terraform-demo"
+    Purpose = "terraform-practice"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls_1"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
